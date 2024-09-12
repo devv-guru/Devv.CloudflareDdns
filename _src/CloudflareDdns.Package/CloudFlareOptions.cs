e79ba336cc029d7c499a5913850ae48a4fc4a52c
Deon van Vuuren <deon@vanfmly.com>
2024-09-12 12:02:03 +0100

    Update CloudFlareOptions.cs

--- _src/CloudflareDdns.Package/CloudFlareOptions.cs
+++ _src/CloudflareDdns.Package/CloudFlareOptions.cs
@@ -6,12 +6,12 @@ public class CloudFlareOptions
 
     public required string? Email { get; set; }
     public required string? Key { get; set; }
-    public Records[] Records { get; set; }
+    public Records[]? Records { get; set; }
 }
 
 public class Records
 {
-    public string? ZoneId { get; set; }
-    public string? DnsRecordId { get; set; }
-    public string? Name { get; set; }
+    public required string? ZoneId { get; set; }
+    public required string? DnsRecordId { get; set; }
+    public required string? Name { get; set; }
 }
