ee24e5403ea7ed8174dd1176ae3d7e48d87da1fb
Deon van Vuuren <deon@vanfmly.com>
2025-01-21 19:36:14 +0000

    Added Seq logging
    Added Seq logging

--- _src/CloudflareDdns.Server/Program.cs
+++ _src/CloudflareDdns.Server/Program.cs
@@ -1,5 +1,6 @@
 using Azure.Identity;
 using Devv.CloudflareDdns;
+using Serilog;
 
 namespace CloudflareDdns.Server;
 
@@ -7,49 +8,69 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        var builder = WebApplication.CreateBuilder(args);
-        var config = builder.Configuration;
+        Log.Logger = new LoggerConfiguration()
+            .WriteTo.Console()
+            .CreateLogger();
 
-        var clientId = config["AZURE_CLIENT_ID"];
-        var secret = config["AZURE_CLIENT_SECRET"];
-        var tenantId = config["AZURE_TENANT_ID"];
-        var vaultUrl = config["VAULT_URL"];
-
-        if (string.IsNullOrEmpty(clientId))
+        try
         {
-            throw new InvalidOperationException("VAULT_CLIENT_ID is not set!");
-        }
+            var builder = WebApplication.CreateBuilder(args);
+            var config = builder.Configuration;
 
-        if (string.IsNullOrEmpty(secret))
+            var clientId = config["AZURE_CLIENT_ID"];
+            var secret = config["AZURE_CLIENT_SECRET"];
+            var tenantId = config["AZURE_TENANT_ID"];
+            var vaultUrl = config["VAULT_URL"];
+
+            if (string.IsNullOrEmpty(clientId))
+            {
+                throw new InvalidOperationException("VAULT_CLIENT_ID is not set!");
+            }
+
+            if (string.IsNullOrEmpty(secret))
+            {
+                throw new InvalidOperationException("VAULT_CLIENT_SECRET is not set!");
+            }
+
+            if (string.IsNullOrEmpty(vaultUrl))
+            {
+                throw new InvalidOperationException("VAULT_URL is not set!");
+            }
+
+            builder.Configuration.AddAzureKeyVault(new Uri(vaultUrl), new DefaultAzureCredential());
+
+            builder.Services.AddSerilog((services, lc) =>
+                lc.ReadFrom.Configuration(builder.Configuration)
+                .Enrich.FromLogContext()
+                .WriteTo.Console());
+
+            builder.Services.AddAuthorization();
+
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+            builder.Services.AddCloudflareDynamicDns(config);
+
+            var app = builder.Build();
+
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+            app.Run();
+        }
+        catch (Exception ex)
         {
-            throw new InvalidOperationException("VAULT_CLIENT_SECRET is not set!");
+            Log.Fatal(ex, "Application terminated unexpectedly");
         }
-
-        if (string.IsNullOrEmpty(vaultUrl))
+        finally
         {
-            throw new InvalidOperationException("VAULT_URL is not set!");
+            Log.CloseAndFlush();
         }
-
-        builder.Configuration.AddAzureKeyVault(new Uri(vaultUrl), new DefaultAzureCredential());
-
-        builder.Services.AddAuthorization();
-        
-        builder.Services.AddEndpointsApiExplorer();
-        builder.Services.AddSwaggerGen();
-        builder.Services.AddCloudflareDynamicDns(config);
-
-        var app = builder.Build();
-        
-        if (app.Environment.IsDevelopment())
-        {
-            app.UseSwagger();
-            app.UseSwaggerUI();
-        }
-
-        app.UseHttpsRedirection();
-
-        app.UseAuthorization();
-
-        app.Run();
     }
 }
